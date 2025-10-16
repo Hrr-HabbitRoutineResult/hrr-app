a6e8291c7d76b1d256e56f472e6293c2e384b0a5
modzivv <vvgkxm@gmail.com>
2025-10-16 21:39:26 +0900

    feat: 스플래시 화면 구현 #3

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import React, { useEffect } from 'react';
+import BootSplash from 'react-native-bootsplash';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    // 앱 초기화 작업
+    const init = async () => {
+      // TODO: 실제 초기화 작업 추가하기
+      await new Promise((resolve) => setTimeout(() => resolve(undefined), 2000)); // 최소 2초간은 스플래시 화면 유지
+    };
+
+    // 초기화 완료 후에는 스플래시 화면 부드럽게 숨기기
+    init().finally(() => {
+      BootSplash.hide({ fade: true });
+    });
+  }, []);
 
   return (
     <SafeAreaProvider>
