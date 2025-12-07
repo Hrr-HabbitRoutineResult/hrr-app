1227df09b3c0b22ca9881be602a205f937ddcd93
modzivv <vvgkxm@gmail.com>
2025-12-08 04:32:29 +0900

    feat: 추천 챌린지 카드에 썸네일 적용 및 프로필 화면 이동 처리 #22

--- declarations.d.ts
+++ declarations.d.ts
@@ -4,3 +4,14 @@ declare module '*.svg' {
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+declare module 'react-native-config' {
+  export interface NativeConfig {
+    API_BASE_URL?: string;
+    S3_BUCKET_NAME?: string;
+    S3_REGION?: string;
+  }
+
+  export const Config: NativeConfig;
+  export default Config;
+}
