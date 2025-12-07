378c2337084adbece7c92d0b718b144ed7f2e7a8
modzivv <vvgkxm@gmail.com>
2025-12-08 05:17:33 +0900

    feat: 채팅/마이페이지 빈 화면 UI 추가 #22

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { colors, typography } from '../design/tokens';
+import LogoGray from '../../assets/images/logo-gray.svg';
 
 const ChatScreen = () => (
   <View style={styles.container}>
-    <Text>Chat Screen</Text>
+    <View style={styles.emptyContainer}>
+      <LogoGray width={124.16} height={119.79} />
+      <Text style={styles.emptyText}>아직 준비 중이에요.</Text>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.background,
+  },
+  emptyContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  emptyText: {
+    ...typography.smReg,
+    color: colors.icon.gray,
+    textAlign: 'center',
+    lineHeight: 21,
+    marginTop: 32,
   },
 });
 
